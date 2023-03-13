5370e1d94a64c1c067c7b138be458adb7355c2c3
xavier <123534921+xaaavier484@users.noreply.github.com>
2023-03-13 08:03:06 +0300

    Initial Commit

---
+++ xavierchat/chat/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from chat.models import Room, Message
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
+    return render(request, 'room.html',{
+        'username': username,
+        'room_details': room_details
+    })
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
